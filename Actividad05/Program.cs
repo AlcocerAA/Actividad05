004f8ceb4c3b3f89f1e7a2e7205e56979b250772
bennndev <benjamin.sullca1103@gmail.com>
2026-09-14 10:00:40 -0500

    chore: configuración de Swagger en Program.cs

--- Actividad05/Program.cs
+++ Actividad05/Program.cs
@@ -1,16 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseExceptionHandler("/Home/Error");
+    
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Actividad 05");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
-
