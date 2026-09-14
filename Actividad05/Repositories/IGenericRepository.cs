d7205847f48294ec0f3538bf30a62aa263f5dce7
Alex <alexander.vasquezmontes@gmail.com>
2026-09-14 09:39:00 -0500

    chore: upload repository structure

---
+++ Actividad05/Repositories/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab04_AlexanderVasquez.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAll();
+    Task<T?> GetById(int id);
+    Task Add(T entity);
+    void Update(T entity);
+    Task Delete(int id);
+}
